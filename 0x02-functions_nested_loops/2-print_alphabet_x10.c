f30db6a69825972c3f7eab12b46ab671039e8347
victor nwafor <victorokwy29@gmail.com>
2022-09-14 19:17:01 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int circle = 0;
+	char letter = 'a';
+
+	while (circle < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+
+		round++;
+	}
+}
