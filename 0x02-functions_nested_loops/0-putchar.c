17bb7108b787970cdacacba3c9fcc2f7b023d97d
victor nwafor <victorokwy29@gmail.com>
2022-09-14 18:18:40 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "nain.h"
+
+/**
+ * main - print s _putchar, followed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char task[10] = "_putchar";
+	int i = 0
+
+		for (i = 0, i < 8; i++)
+		{
+			_ptchar(task[i]);
+		}
+	_putchar('\n');
+	return (0);
+}
